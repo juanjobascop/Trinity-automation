03b07c1602a814319a35773d89b101e8b3f71ce7
juanjobascop <juanjobascope@gmail.com>
2025-11-20 09:44:45 -0400

    updated comments
    add initial contractor management test

--- cypress/e2e/contractorTest.cy.js
+++ cypress/e2e/contractorTest.cy.js
@@ -6,9 +6,8 @@ describe('Contractor Management Test', () => {
     cy.visit('http://154.38.173.164:6980');
     cy.wait(2000); // Wait for 1 second to ensure the page loads
 
-    // 2. Log in
-    // Note: I am guessing the specific ID of the input boxes based on standard practices.
-    // If these fail, we will use the "Selector Playground" to find the real ones.
+
+    
     
     // Finds the input for username and types 'admin'
     cy.get('input[name="username"]').type('admin'); 
@@ -19,7 +18,7 @@ describe('Contractor Management Test', () => {
     // Finds the button with type="submit" and clicks it
     cy.get("button[type='button']").click();
     cy.wait(3000);
-    // 3. Wait for dashboard to load (Optional but good practice)
+ 
     // We verify the URL changed or we see the dashboard
     cy.url().should('not.include', 'login');
 
@@ -31,14 +30,13 @@ describe('Contractor Management Test', () => {
     cy.get("div[class='w-full flex items-center gap-2']").click();
     cy.wait(2000); // Wait for 2 seconds to ensure the modal opens
     // 6. Verify the Modal is actually open
-    // We check if the page now contains the header text of the modal
-    //cy.contains('Create new contractor').should('be.visible');
+
 
 
     //We will check field by field if the locators are correct
     cy.get('#firstName').type('CypressFirstName');
     cy.get('#lastName').type('CypressLastName');
-    cy.pause(); 
+  
     //cy.get('#status-active').check(); // Status radio button is not working, the radio buttons appear hidden in cypress
     cy.contains('ACTIVE').click({ force: true });
     
